30e1034042bae94fa5b372c91e3cdb7fd6b4ee47
Shishir <srajen2018@gmail.com>
2023-04-29 16:05:13 -0400

    auto pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,26 @@
 package frc.robot;
 
+import java.util.HashMap;
+import java.util.List;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import frc.robot.autos.*;
 import frc.robot.commands.*;
+import frc.robot.constants.AutoConstants;
+import frc.robot.constants.Constants;
 import frc.robot.subsystems.*;
 
 /**
@@ -33,6 +45,30 @@ public class RobotContainer {
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
 
+    public static HashMap<Command, String> autoMap = new HashMap<>();
+
+    public Command sAutoBuilder(String pathName, HashMap<String, Command> eventMap) {
+        SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+            s_Swerve::getPose, // Pose2d supplier
+            s_Swerve::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
+            Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
+            new PIDConstants(5.0, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+            new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+            s_Swerve::setModuleStates, // Module states consumer used to output to the drive subsystem
+            eventMap,
+            true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+            s_Swerve);
+        
+        List<PathPlannerTrajectory> pathToFollow = PathPlanner.loadPathGroup(pathName,
+            PathPlanner.getConstraintsFromPath(pathName));
+        final Command auto = autoBuilder.fullAuto(pathToFollow);
+        autoMap.put(auto, pathName);
+        return auto;
+    }
+
+    public static SendableChooser<Command> chooser = new SendableChooser<>();
+    
+
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -46,8 +82,22 @@ public class RobotContainer {
             )
         );
 
+        setOffseasonPath();
+
+        Shuffleboard.getTab("AUTON").add(chooser).withSize(3, 1);
+        Command instantCmd = new InstantCommand();
+        chooser.setDefaultOption("Nothing", instantCmd);
+        chooser.addOption("Offseason Path", sAutoBuilder("Offseason Path", AutoConstants.offseasonPath));
+        chooser.addOption("Example Path", new exampleAuto(s_Swerve));
+        autoMap.put(instantCmd, "nothing");
+
         // Configure the button bindings
         configureButtonBindings();
+    }
+
+    public void setOffseasonPath() {
+        AutoConstants.offseasonPath.put("Start", new InstantCommand());
+        AutoConstants.offseasonPath.put("Stop", new InstantCommand());
     }
 
     /**
@@ -68,6 +118,6 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve);
+        return chooser.getSelected();
     }
 }
---
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+import java.util.HashMap;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AutoConstants {
+    public static final HashMap<String, Command> offseasonPath = new HashMap<>();
+}
